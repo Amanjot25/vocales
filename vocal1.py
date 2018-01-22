e15a0ac5a9267445c847539532e1ff7aaffe6605
ism23819737 <ism23819737@a24.informatica.escoladeltreball.org>
2018-01-22 13:52:10 +0100

    First Load

---
+++ vocal1.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+letra = int(input("Dime una letra"))
+if letra!="a" or letra!="e" or letra!="i" or letra!="o" or letra!="u":
+	print "Es una vocal"
+else:
+	print "No es consonante"
+if (letra!="a") and (letra!="z"):
+	print "Es una consonante"
+else:
+	print "No es vocal"
